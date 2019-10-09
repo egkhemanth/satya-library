53db35a4b1d4c61fc5ad7b81d27ee3452b05dbd7
satya <satyaupendra.devabhaktineni9@gmail.com>
2019-10-09 23:38:32 +0530

    Updates

---
+++ precache-manifest.3b55e5ec60ae37b81f0e14e565d76d44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d88a93a7d52fbcac3ee52787c9fc88",
+    "url": "/satya-library/index.html"
+  },
+  {
+    "revision": "c742fae511c5a53491f5",
+    "url": "/satya-library/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "27cf60eb0fdba90a0c65",
+    "url": "/satya-library/static/css/main.47121dd5.chunk.css"
+  },
+  {
+    "revision": "c742fae511c5a53491f5",
+    "url": "/satya-library/static/js/2.ebf944bc.chunk.js"
+  },
+  {
+    "revision": "27cf60eb0fdba90a0c65",
+    "url": "/satya-library/static/js/main.cd9d50c1.chunk.js"
+  },
+  {
+    "revision": "4adaa8c673b5cb57628c",
+    "url": "/satya-library/static/js/runtime-main.1faae2ca.js"
+  },
+  {
+    "revision": "76f3d7204a22276ad8013deecea70c7c",
+    "url": "/satya-library/static/media/logo.76f3d720.svg"
+  }
+]);
